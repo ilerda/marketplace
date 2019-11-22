d389ca4d2243b009226815258d93211585bf0ef2
Alex <alecroberts92@gmail.com>
2019-11-22 12:00:44 +0000

    added two files for basic unit testing

---
+++ tests/test_get.py
@@ -0,0 +1,6 @@
+'''
+Unit testing script for get method
+Make sure you run:
+python3 app.py
+first!!
+'''
---
+++ tests/test_post_delete.py
@@ -0,0 +1,6 @@
+'''
+Unit testing script for post and delete methods
+Make sure you run:
+python3 app.py
+first!!
+'''
