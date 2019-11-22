7148fa879a91f8f3d841e944dce17d643b303ca6
Alex <alecroberts92@gmail.com>
2019-11-22 10:02:44 +0000

    app config and product class, which defines the db

--- app.py
+++ app.py
@@ -2,3 +2,16 @@
 
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    price = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return '<Product %r>' % self.name
